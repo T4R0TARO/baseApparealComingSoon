c41e45c82929688a592232bf0bb5b9974184df75
josh manansala <joshmanansalagsd@gmail.com>
2021-09-06 16:41:42 -0700

    fix merge conflict

--- js/script.js
+++ js/script.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
+
 // captured variables
 const emailButton = document.getElementById('emailButton');
 
 
 // checks user input and test for email validation 
-=======
-console.log('wah');
-const emailButton = document.getElementById('emailButton');
-
-
-// check input for email validation 
->>>>>>> a8903b44d3265249e70745be56803a510a8ae601
  function emailValidation() {
      const emailInput  = document.getElementById('emailInput').value;
      const regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
@@ -24,8 +17,5 @@ const emailButton = document.getElementById('emailButton');
      }
  }
 
-<<<<<<< HEAD
 // Waits for click to run function emailValidation()
-=======
->>>>>>> a8903b44d3265249e70745be56803a510a8ae601
  emailButton.addEventListener('click', emailValidation)
