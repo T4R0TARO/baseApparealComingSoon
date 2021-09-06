745708ab7c78a390e86002a7dca28453a4aa4118
josh manansala <joshmanansalagsd@gmail.com>
2021-09-06 16:15:59 -0700

    test

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log('wah');
+// captured variables
+const emailButton = document.getElementById('emailButton');
+
+
+// checks user input and test for email validation 
+ function emailValidation() {
+     const emailInput  = document.getElementById('emailInput').value;
+     const regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
+     const errorContainer = document.querySelector('.errorContainer');
+
+     if (regx.test(emailInput)){
+         alert("thank you");
+         errorContainer.className = "errorContainer";
+     } else {
+         errorContainer.className = "errorContainerShow";
+     }
+ }
+
+// Waits for click to run function emailValidation()
+ emailButton.addEventListener('click', emailValidation)
